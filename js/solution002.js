374c91b39da0e7073f6330e58c6ef7f92349f367
Taylor <themacnair@hotmail.com>
2022-06-03 18:25:30 -0400

    6.3.2022 CW

---
+++ js/solution002.js
@@ -0,0 +1,8 @@
+function paperwork(n, m) {
+    if (m <= 0 || n <= 0) {
+      return 0;
+    }
+    return m * n;
+  }
+
+// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/solutions/javascript
