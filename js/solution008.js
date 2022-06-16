b1136781573d7e18f79ee8bd5a766909eb013c66
Taylor <themacnair@hotmail.com>
2022-06-15 20:51:41 -0400

    CodeWars 6.15.2022

---
+++ js/solution008.js
@@ -0,0 +1,8 @@
+function lovefunc(flower1, flower2){
+    if(flower1%2!==flower2%2){
+      return true;
+    } else {
+      return false;
+    }
+  }
+//https://www.codewars.com/kata/555086d53eac039a2a000083/solutions/javascript
